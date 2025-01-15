e46426bd28c00fcd189a9d892bb32042fb05c94f
Wellsaik <artem.podorozhko@outlook.com>
2025-01-15 20:44:58 +0200

    UMenuPluginBlueprintLibrary::ClearObject(UObject*& InObjectRef)

--- Source/HumMenuPlugin/Private/Libs/MenuPluginBlueprintLibrary.cpp
+++ Source/HumMenuPlugin/Private/Libs/MenuPluginBlueprintLibrary.cpp
@@ -1,5 +1,6 @@
 #include "Libs/MenuPluginBlueprintLibrary.h"
 
+#include "Blueprint/UserWidget.h"
 #include "Data/WidgetPropertiesSettings.h"
 #include "Engine/Engine.h"
 #include "UObject/ConstructorHelpers.h"
@@ -13,4 +14,34 @@ TSoftObjectPtr<UWidgetsPropertiesDataAsset> UMenuPluginBlueprintLibrary::GetWidg
 		return da;
 	}
 	return TSoftObjectPtr<UWidgetsPropertiesDataAsset>();
+}
+
+void UMenuPluginBlueprintLibrary::ClearObject(UObject*& InObjectRef)
+{
+	// Exit if the object reference is invalid.
+	if (!IsValid(InObjectRef))
+	{
+		return; 
+	}
+
+	if (AActor* actor = Cast<AActor>(InObjectRef))
+	{
+		// Destroy the actor.
+		actor->Destroy();
+		
+		// Clear the reference.
+		InObjectRef = nullptr; 
+		return;
+	}
+
+	if (UUserWidget* userWidget = Cast<UUserWidget>(InObjectRef))
+	{
+		// Remove the user widget from its parent.
+		userWidget->RemoveFromParent(); 
+	}
+
+	// Mark the object for destruction.
+	InObjectRef->ConditionalBeginDestroy(); 
+	// Clear the reference.
+	InObjectRef = nullptr;
 }
--- Source/HumMenuPlugin/Public/Libs/MenuPluginBlueprintLibrary.h
+++ Source/HumMenuPlugin/Public/Libs/MenuPluginBlueprintLibrary.h
@@ -14,9 +14,23 @@ UCLASS()
 class HUMMENUPLUGIN_API UMenuPluginBlueprintLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
-	
+
 public:
-	
-    UFUNCTION(BlueprintCallable, Category = "Widget Settings")
+	UFUNCTION(BlueprintCallable, Category = "MenuPluginLibrary|Widget Settings")
 	static TSoftObjectPtr<UWidgetsPropertiesDataAsset> GetWidgetPropertiesDataAsset();
+
+	/**
+	 * Clears the provided object reference by destroying or removing it appropriately.
+	 *
+	 * - If the object is an actor, it is destroyed 
+	 * - If the object is a user widget, it is removed from its parent and marked for destruction (RAM Clear).
+	 * - If the object is any other type, it is marked for destruction (RAM Clear)..
+	 *
+	 * @note In either case, the output variable will be nullptr so that your UPROPERTY variable does not point to an invalid object
+	 * @warning The input object parameter must be valid (not in the process of deletion), otherwise the function will fail even if the pointer points to the some object!
+	 *
+	 * @param InObjectRef A reference to the UObject to be cleared. This parameter must be passed by reference.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "MenuPluginLibrary|Misc")
+	static void ClearObject(UPARAM(Ref, DisplayName="Object (Reference)")UObject*& InObjectRef);
 };
